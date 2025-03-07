f206c77320ad75fa09791712fae7aa1ac6acc530
jagadeeshram1119 <163256381+jagadeeshram1119@users.noreply.github.com>
2025-03-07 13:24:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-var cart = document.getElementById("cart");
+var card = document.getElementById("card");
 var btn = document.getElementById("generatorbtn");
-btn.addEventListener("click ");
+
+function generator() {
+  var cardIteam = document.createElement("div");
+  cardIteam.className = "carditeam";
+  var cardImg = document.createElement("img");
+  cardImg.className = "cardimg";
+  cardImg.src = "https://avatars.githubusercontent.com/u/106391470?v=4";
+
+  var cardTitle = document.createElement("h3");
+  cardTitle.textContent = "Jagadeesh Ram";
+  var cardDec = document.createElement("p");
+  cardDec.textContent =
+    "simple code to get this profile by using html,css and js";
+  cardIteam.appendChild(cardImg);
+  cardIteam.appendChild(cardTitle);
+  cardIteam.appendChild(cardDec);
+  card.appendChild(cardIteam);
+}
+btn.addEventListener("click", generator);
